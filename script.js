9bcaed089a73b685fd337f1987d302d1692275a5
IGI-29 <60250274+IGI-29@users.noreply.github.com>
2020-04-16 21:41:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function validate()
+ {
+ var username=document.getElementById("username");
+ var name=document.getElementById("name");
+ var password=document.getElementById("password");
+ var email=document.getElementById("email");
+ if(name.value=="")
+	 alert("please enter some value");
+ if(username.value=="")
+	 alert("please enter some value");
+ if(email.value=="")
+	 alert("please enter some value");
+ if(password.value=="")
+	 alert("please enter some value");
+ }
